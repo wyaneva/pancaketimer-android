67b451744c75330552d9a91cae80ad32329c535c
Vanya Yaneva <wyaneva@gmail.com>
2020-05-21 22:24:01 +0100

    Added a button!

--- app/src/main/java/com/example/pancaketimer/MainActivity.java
+++ app/src/main/java/com/example/pancaketimer/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.pancaketimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button_Hello);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final TextView textView_Hello = findViewById(R.id.textView_Hello);
+                final EditText editText_Name = findViewById(R.id.editText_Name);
+
+                textView_Hello.setText("Hello " + editText_Name.getText() + "!");
+            }
+        });
     }
 }
