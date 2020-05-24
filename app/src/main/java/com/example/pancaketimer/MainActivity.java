b6f26ac240ed25abc567397d73494e7608cd0749
Vanya Yaneva <wyaneva@gmail.com>
2020-05-24 18:05:57 +0100

    Added a progress bar

--- app/src/main/java/com/example/pancaketimer/MainActivity.java
+++ app/src/main/java/com/example/pancaketimer/MainActivity.java
@@ -5,24 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    void startTimer() {
+        final ProgressBar progress_countdown = findViewById(R.id.progress_countdown);
+        progress_countdown.setProgress(50);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setIcon(R.drawable.ic_timer);
+        getSupportActionBar().setTitle("Pancake Timer");
 
-        final Button button = findViewById(R.id.button_Hello);
+        final Button button = findViewById(R.id.button_Start);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                final TextView textView_Hello = findViewById(R.id.textView_Hello);
-                final EditText editText_Name = findViewById(R.id.editText_Name);
-
-                textView_Hello.setText("Hello " + editText_Name.getText() + "!");
+                startTimer();
+                final TextView textView_countdown= findViewById(R.id.textView_countdown);
+                textView_countdown.setText("Hello!");
             }
         });
     }
