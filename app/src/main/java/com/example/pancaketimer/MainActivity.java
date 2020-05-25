ae0670075d24046dca4ffd140fe5cd5f25033ef6
Vanya Yaneva <wyaneva@gmail.com>
2020-05-25 19:41:06 +0100

    Added a basic timer

--- app/src/main/java/com/example/pancaketimer/MainActivity.java
+++ app/src/main/java/com/example/pancaketimer/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.pancaketimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -11,9 +13,28 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    void startTimer() {
+    void startTimer(final long total_ms, final MediaPlayer mp) {
         final ProgressBar progress_countdown = findViewById(R.id.progress_countdown);
-        progress_countdown.setProgress(50);
+        final TextView textView_countdown= findViewById(R.id.textView_countdown);
+
+        new CountDownTimer(total_ms, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                long sUntilFinished = millisUntilFinished / 1000 + 1; // dispay n to 1, not n-1 to 0
+                long total_s = total_ms / 1000;
+                long sElapsed = total_s - sUntilFinished;
+                int progress = (int)((sElapsed*100)/total_s);
+
+                textView_countdown.setText(sUntilFinished + "/" + total_s);
+                progress_countdown.setProgress(100-progress);
+            }
+
+            public void onFinish() {
+                textView_countdown.setText("Flip!");
+                progress_countdown.setProgress(0);
+                mp.start();
+            }
+        }.start();
     }
 
     @Override
@@ -22,13 +43,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().setIcon(R.drawable.ic_timer);
         getSupportActionBar().setTitle("Pancake Timer");
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.beep);
 
         final Button button = findViewById(R.id.button_Start);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                startTimer();
-                final TextView textView_countdown= findViewById(R.id.textView_countdown);
-                textView_countdown.setText("Hello!");
+                startTimer(10000, mp);
             }
         });
     }
