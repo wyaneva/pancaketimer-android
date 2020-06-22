605f14f54db1e67ce826021dedc334fced9dd9aa
Vanya Yaneva <wyaneva@gmail.com>
2020-06-22 09:50:32 +0100

    Added radio buttons enabling in Settings

--- app/src/main/java/com/example/pancaketimer/SettingsActivity.java
+++ app/src/main/java/com/example/pancaketimer/SettingsActivity.java
@@ -2,25 +2,42 @@ package com.example.pancaketimer;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.pancaketimer.util.PrefUtil;
 
-import org.w3c.dom.Text;
-
 public class SettingsActivity extends AppCompatActivity {
 
     void finishActivityOK() {
         setResult(Activity.RESULT_OK);
         finish();
+    }
+
+    void enableRadioButtons() {
+        final RadioButton radioButtonPancake = findViewById(R.id.radioButton_pancakemode);
+        final RadioButton radioButtonCrepe = findViewById(R.id.radioButton_crepemode);
+        final RadioButton radioButtonCustom = findViewById(R.id.radioButton_custommode);
+        final EditText editTextSide1 = findViewById(R.id.editText_Side1);
+        final EditText editTextSide2 = findViewById(R.id.editText_Side2);
+
+        if(radioButtonPancake.isChecked() || radioButtonCrepe.isChecked()) {
+            editTextSide1.setEnabled(false);
+            editTextSide2.setEnabled(false);
+            return;
+        }
+
+        if(radioButtonCustom.isChecked()) {
+            editTextSide1.setEnabled(true);
+            editTextSide2.setEnabled(true);
+            return;
+        }
     }
 
     @Override
@@ -35,6 +52,7 @@ public class SettingsActivity extends AppCompatActivity {
         final RadioButton radioButtonCrepe = findViewById(R.id.radioButton_crepemode);
         final RadioButton radioButtonCustom = findViewById(R.id.radioButton_custommode);
 
+        // Initialise controls
         int mode = PrefUtil.getMode(this);
         switch (mode){
             case PrefUtil.MODE_PANCAKE:
@@ -49,14 +67,27 @@ public class SettingsActivity extends AppCompatActivity {
         }
 
         int time1 = PrefUtil.getSide1Seconds(this);
-        final EditText editText_side1 = findViewById(R.id.editText_Side1);
-        editText_side1.setText(Integer.toString(time1));
+        final EditText editTextSide1 = findViewById(R.id.editText_Side1);
+        editTextSide1.setText(Integer.toString(time1));
         int time2 = PrefUtil.getSide2Seconds(this);
-        final EditText editText_side2 = findViewById(R.id.editText_Side2);
-        editText_side2.setText(Integer.toString(time2));
+        final EditText editTextSide2 = findViewById(R.id.editText_Side2);
+        editTextSide2.setText(Integer.toString(time2));
+        enableRadioButtons();
 
-        final Button button = findViewById(R.id.button_save);
-        button.setOnClickListener(new View.OnClickListener() {
+        // Set listener on radio buttons
+        CompoundButton.OnCheckedChangeListener listener = new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                enableRadioButtons();
+            }
+        };
+        radioButtonPancake.setOnCheckedChangeListener(listener);
+        radioButtonCrepe.setOnCheckedChangeListener(listener);
+        radioButtonCustom.setOnCheckedChangeListener(listener);
+
+        // Set listener on Save button
+        final Button buttonSave = findViewById(R.id.button_save);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
                 if(radioButtonPancake.isChecked()) {
@@ -73,10 +104,8 @@ public class SettingsActivity extends AppCompatActivity {
 
                 if(radioButtonCustom.isChecked()) {
                     PrefUtil.setMode(PrefUtil.MODE_CUSTOM, SettingsActivity.this);
-                    final EditText editText_side1 = findViewById(R.id.editText_Side1);
-                    final EditText editText_side2 = findViewById(R.id.editText_Side2);
-                    String side1_str = editText_side1.getText().toString();
-                    String side2_str = editText_side2.getText().toString();
+                    String side1_str = editTextSide1.getText().toString();
+                    String side2_str = editTextSide2.getText().toString();
                     if(side1_str.isEmpty() || side2_str.isEmpty()) {
                         CharSequence text = "Please, enter custom times \nfor Side 1 and Side 2.";
                         int duration = Toast.LENGTH_SHORT;
